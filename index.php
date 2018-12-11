3664654dbd85654d9936b86300a2e934e4df1c4d
LucianMatrix17 <benardcharumbira@gmail.com>
2018-12-11 18:32:51 +0200

    made changes to about and gallery left with contact form

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+
+
+<html lang="en" style="width: 100%;">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Charmonman|Satisfy|Varela+Round" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+    <link href="css/grayscale.css" rel="stylesheet">
+    <link href="css/gallery_style.css" rel="stylesheet">
+
+
+    <!-- font awesome icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <!--   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ -->
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><i class="fas fa-moon"></i> hild <strong id="visuals">Visuals</strong></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#projects">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#slideshow">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+<!--     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#projects">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#slideshow">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+ -->
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+
+          <h1 class="text-center" >We are Moon Child Visuals</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">a one stop solution for all your creative needs</h2>
+        </div>
+      </div>
+    </header>
+
+    <!--  <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>We are Moon Child Visuals</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">a one stop solution for all your creative needs</p>
+          </div>
+        </div>
+      </div>
+    </header> -->
+
+    <!-- About Section -->
+
+    <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-third w3-center">
+      <i class="fa fa-group w3-padding-64 w3-text-blue w3-margin-right" style="font-size: 200px;"></i>
+    </div>
+
+    <div class="w3-twothird">
+      <h1>ABOUT</h1>
+      <h5 class="w3-padding-32">We are a visuals company founded on the principle that creativity can be expressed in many forms. We strive to produce content that is complex yet simple in its beauty.</h5>
+
+      <p class="w3-text-grey"><cite>We bring your wildest ideas to life. </cite> <br> 
+               <i>-Krazy Moon Child (founder)</i> 
+      </p>
+      
+    </div>
+  </div>
+</div>
+
+<!--     <section id="about" class="align-items-center d-flex about-sect bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <img id="aboutImage" class="mb-3 mb-lg-0 animAbout" src="img/boy.png" alt="aboutImage">
+          </div>
+          <div class="col-lg-8 align-items-center">
+            <div class="featured-text text-center text-lg-left">
+              <h1 class="text-center">ABOUT</h4>
+              <p class="text-black-50 text-center mb-0">We are a visuals company founded on the principle that creativity can be expressed in many forms. We strive to produce content that is complex yet simple in its beauty.</p>
+              <br>
+              <p class="text-center" ><cite>We bring your wildest ideas to life. </cite> <br> 
+               <i>-Krazy Moon Child (founder)</i> 
+              </p>
+            </div>
+          </div>
+        </div>
+    
+
+      </div>
+    </section> -->
+
+    
+
+    <!-- Services Section -->
+   
+    <section id="projects" class="projects-section bg-light">
+      <h4 class="text-center mb-5">SERVICES</h4>
+      <div class="container">
+
+     
+       
+         <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid mr-0" src="img/2.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Photography</h4>
+                  <p class="mb-0 text-white-50">We offer shoots for weddings, birthdays, 
+                  parties, corporate events and various other functions.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="img/3.jpg" alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white">Videography</h4>
+                  <p class="mb-0 text-white-50">We provide video shoots: music videos, drama, marketing videos, club promotions etc.  </p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Gallery section -->
+<!--     <section id="slideshow">
+      <h4 class="text-center" >GALLERY</h4>
+      <div class="entire-content">
+        <div class="content-carrousel">
+          <figure class="shadow"><img src="img/slider/3.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/1.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/10.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/7.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/8.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/6.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/11.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/4.jpg"/></figure>
+          <figure class="shadow"><img src="img/slider/9.jpg"/></figure>
+          
+    </div>
+  </div>
+</section> -->
+
+    
+    
+
+    <!-- Signup Section -->
+    <section id="signup" class="signup-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+            <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+            <h2 class="text-white mb-5">Get quote!</h2>
+
+            <form name="contactform" method="post" action="contact.php" class="form-block d-flex">
+              <td>
+               <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" name="name" placeholder="Enter name...">   
+              </td>
+              
+            <br>
+            
+              <input  class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" name="email" placeholder="Enter email address...">
+            
+            <br>
+           
+              <input  class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" name="details" placeholder="Enter photoshoot details...">
+            </form>
+
+            <button type="submit" value="submit" class="btn btn-primary mx-auto" id="sendButton"> <a class="text-white" href="../contact.php">Send</a></button>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact-section bg-black">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Location</h4>
+                <hr class="my-4">
+                <div class="small text-black-10">South Africa, Gauteng</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-envelope text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">
+                  <a href="#">benardcharumbira@gmail.com</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Phone</h4>
+                <hr class="my-4">
+                <div class="small text-black-10">+27 83 549 0521</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="social d-flex justify-content-center">
+          <a href="https://www.instagram.com/krazy_moon_child/" class="mx-2">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+         &copy; Moon Child Visuals 2018 site by: <a href="https://www.linkedin.com/in/benard-charumbira-62a77711b">  Benard Charumbira</a>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/grayscale.min.js"></script>
+
+  </body>
+
+</html>
